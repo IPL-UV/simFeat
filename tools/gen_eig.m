c0a078de21100b52c69596d68af592343a5a4656
jordi <jordi@cf90e129-9db2-4be2-879b-6cf61410cf68>
2016-04-22 15:18:47 +0000

    A trivial optimization in gen_eig.m to avoid repeating the same code.
    git-svn-id: https://harpo.uv.es/repos/ipl/Software@633 cf90e129-9db2-4be2-879b-6cf61410cf68

--- tools/gen_eig.m
+++ tools/gen_eig.m
@@ -1,4 +1,4 @@
-function [U D] = gen_eig(A, B, n_eig)
+function [U, D] = gen_eig(A, B, n_eig)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Extracts generalized eigenvalues for problem A * U = B * U * Lambda
@@ -15,33 +15,24 @@ OPTS.disp = 0;
 B = (B + B') / 2;
 R = size(B,1);
 rango = rank(B);
-if (rango == R)
-    U = zeros(R, n_eig);
-    D = zeros(n_eig, n_eig);
-    % inv_B = inv(B);
-    for k = 1:n_eig
-        % [a,d] = eigs(inv_B*A,1, 'LM', OPTS);
-        [a,d] = eigs(B \ A, 1, 'LM', OPTS);
-        a = a ./ sqrt(a' * B * a);
-        U(:,k) = a;
-        D(k,k) = d;
-        A = A - d * (B * a) * (a' * B);
-    end
-else
-    % rango = max(rango,sum(eig(B)>0) - 5);
+
+if rango ~= R
+    % rango = max(rango, sum(eig(B)>0) - 5);
     [v,~] = eigs(B,rango);
     B = v' * B * v;
     A = v' * A * v;
-    U = zeros(rango, n_eig);
-    D = zeros(n_eig, n_eig);
-    % inv_B = inv(B);
-    for k = 1:n_eig
-        % [a,d] = eigs(inv_B*A, 1, 'LM', OPTS);
-        [a,d] = eigs(B \ A, 1, 'LM', OPTS);
-        a = a ./ sqrt(a' * B * a);
-        U(:,k) = a;
-        D(k,k) = d;
-        A = A - d * (B * a) * (a' * B);
-    end
+end
+
+U = zeros(rango, n_eig);
+D = zeros(n_eig, n_eig);
+for k = 1:n_eig
+    [a,d] = eigs(B \ A, 1, 'LM', OPTS);
+    a = a ./ sqrt(a' * B * a);
+    U(:,k) = a;
+    D(k,k) = d;
+    A = A - d * (B * a) * (a' * B);
+end
+
+if rango ~= R
     U = v * U;
 end
